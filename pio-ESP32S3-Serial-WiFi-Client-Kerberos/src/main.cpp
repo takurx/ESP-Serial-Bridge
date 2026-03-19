3c5c0724ee011f12f8a15030165d876b443d1963
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2026-03-19 17:16:08 +0900

    flow controlなしにしたので、RTS/CTSのpin設定をcomment outした
    flow controlなしにしたので、RTS/CTSのpin設定をcomment outした

--- pio-ESP32S3-Serial-WiFi-Client-Kerberos/src/main.cpp
+++ pio-ESP32S3-Serial-WiFi-Client-Kerberos/src/main.cpp
@@ -96,7 +96,7 @@ void setupUart(int i) {
   // Arduino-ESP32 allows inversion via begin() 6th arg in newer cores; to be safe, set later if needed.
   // UARTS[1], CTS 20 -> USB D-, RTS 19 -> USB D+ だと競合する気がするので、他のピンに割り当てたい -> UARTS[1], CTS 10, RTS 09 にした
   UARTS[i].ser->begin(UARTS[i].baud, SERIAL_8N1, UARTS[i].rx_pin, UARTS[i].tx_pin, UARTS[i].invert);
-  UARTS[i].ser->setPins(UARTS[i].rx_pin, UARTS[i].tx_pin, UARTS[i].cts_pin, UARTS[i].rts_pin);
+  // UARTS[i].ser->setPins(UARTS[i].rx_pin, UARTS[i].tx_pin, UARTS[i].cts_pin, UARTS[i].rts_pin);
   // UARTS[i].ser->setHwFlowCtrlMode(UART_HW_FLOWCTRL_CTS_RTS);
   UARTS[i].ser->setHwFlowCtrlMode(UART_HW_FLOWCTRL_DISABLE); // TODO: flow control handling in software if needed
   UARTS[i].ser->setTimeout(0);
